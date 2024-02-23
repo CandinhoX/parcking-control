502b15ce3796fbea3ca421445697694650e81f9a
candinho <can_dinho7@hotmail.com>
2024-02-23 16:18:09 -0300

    Parcking-control

---
+++ src/main/java/com/api/parckingcontrol/ParckingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.parckingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParckingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParckingControlApplication.class, args);
+	}
+
+}
