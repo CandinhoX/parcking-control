28b330c95e82ee4cdac60c4a48a0a32c4bc73091
candinho <can_dinho7@hotmail.com>
2024-02-26 13:53:14 -0300

    ParkingSpotModel class

--- src/main/java/com/api/parckingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parckingcontrol/ParkingControlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParckingControlApplication {
+public class ParkingControlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ParckingControlApplication.class, args);
+		SpringApplication.run(ParkingControlApplication.class, args);
 	}
 
 }
