b2d90cad29bde3003f4388604c2de0bfef15619e
candinho <can_dinho7@hotmail.com>
2024-02-26 14:24:38 -0300

    adding some annotations and creation of ParkingSpotDto

---
+++ src/main/java/com/api/parckingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,13 @@
+package com.api.parckingcontrol.services;
+
+import com.api.parckingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    @Autowired
+    ParkingSpotRepository parkingSpotRepository;
+
+}
